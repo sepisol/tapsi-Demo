de6e37eac04c37c78ebbff08eba4d13d062005c0
sepi <spdsoli@gmail.com>
2023-02-16 23:32:12 +0330

    create code

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+toggler.addEventListener("click" , (e) =>{
+    navbar.classList.toggle("nav__expanded");
+});
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) =>{
+    tab.addEventListener("click", (e) => {
+        const targeTabContent = document.querySelector(`#${tab.dataset.tabTarget}`);
+        tabs.forEach((tab) => tab.classList.remove("active"));
+        tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+        tab.classList.add("active");
+        targeTabContent.classList.add("active");
+    });
+});
